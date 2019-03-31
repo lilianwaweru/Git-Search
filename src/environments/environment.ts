182bc6b4e636d8502b944bc9fd7337421f7ce575
lilian waweru <lilowesh.lw@gmail.com>
2019-03-31 15:14:32 +0300

    added a form

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiurl:"http://api.github.com/users/",
+  access_token:" 36fdbadcdfa24c0ffd0b1c7c7f4d40bd15336f91"
 };
 
 /*
