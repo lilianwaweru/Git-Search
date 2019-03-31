24c70ffe685f2ff0d5ca988a64d2a15974b9ed5a
lilian waweru <lilowesh.lw@gmail.com>
2019-03-31 15:57:51 +0300

    impoerted http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {user} from '../user';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,7 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
+    HttpClient,
     AppRoutingModule
   ],
   providers: [],
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import {Httpclient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+user:user:
+
 
   constructor() { }
 }
