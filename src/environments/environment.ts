a060001844320c5566b94538851ff83d782238b7
lilian waweru <lilowesh.lw@gmail.com>
2019-04-02 16:56:38 +0300

    change http to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:"http://api.github.com/users/",
+  apiUrl:"https://api.github.com/users/",
   token:"?access_token=97a6dd7cdfe8358c6c2024b0b76c31f4e3271804",
 };
 
